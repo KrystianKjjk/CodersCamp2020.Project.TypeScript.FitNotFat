7276de42abdc90050d2087c3547c4585cdd75731
FIlip <filip.szwedo@gmail.com>
2021-01-23 16:33:11 +0100

    project setup

---
+++ test/App.test.js
@@ -0,0 +1,9 @@
+//sample test
+
+describe('Lightsaber timer', () => {
+
+    test('Sample test', () => {
+       const sampleText = "Hello world!";
+       expect(sampleText).toBe("Hello world!");
+    })
+})
