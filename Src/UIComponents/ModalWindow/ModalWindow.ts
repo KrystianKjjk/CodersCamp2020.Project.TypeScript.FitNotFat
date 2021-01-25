af75ba183b5b70f508eeda2f31e4c25a6b55821c
Konrad <kj.reczek@gmail.com>
2021-01-25 20:25:33 +0100

    Create ModalWindow component

---
+++ Src/UIComponents/ModalWindow/ModalWindow.ts
@@ -0,0 +1,21 @@
+const modalWindow = (component: HTMLElement, closeWindow: () => void) => {
+    const backgroundDiv = document.createElement('div');
+    const modalWindowDiv = document.createElement('div');
+
+    backgroundDiv.appendChild(modalWindowDiv);
+    modalWindowDiv.appendChild(component);
+
+    backgroundDiv.className = 'modal-window-bg';
+    modalWindowDiv.classList.add('modal-window');
+
+    backgroundDiv.addEventListener('click', (e) => {
+        e.stopPropagation();
+        if (e.target === backgroundDiv) {
+            closeWindow();
+        }
+    })
+    
+    return backgroundDiv;
+}
+
+export default modalWindow;
