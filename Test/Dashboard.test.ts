d8acdf4a6f2991311433cbb7dad8347cbe24ae87
Robert <robert.czwartosz@tlen.pl>
2021-01-26 22:06:44 +0100

    implement component

--- Test/Dashboard.test.ts
+++ Test/Dashboard.test.ts
@@ -3,11 +3,23 @@ import dashboard from '../Src/UIComponents/Dashboard/Dashboard';
 describe('Dashboard test', () => {
     test('returns div', () => {
         let overview = document.createElement('div');
+        overview.innerHTML = "Overview";
         let myDiaryFood = document.createElement('div');
+        myDiaryFood.innerHTML = "Food";
         let myDiaryExercises = document.createElement('div');
+        myDiaryExercises.innerHTML = "Exercises";
         let myGoals = document.createElement('div');
+        myGoals.innerHTML = "Goals";
         let myWeights = document.createElement('div');
+        myWeights.innerHTML = "Weights";
         let myProfile = document.createElement('div');
-        expect(dashboard(overview, myDiaryFood, myDiaryExercises, myGoals, myWeights, myProfile)).toBeInstanceOf(HTMLDivElement);
+        myProfile.innerHTML = "Profile";
+        expect(dashboard({
+            'overview': overview, 
+            'diary-food': myDiaryFood, 
+            'diary-exercises': myDiaryExercises, 
+            'goals': myGoals, 
+            'weights': myWeights, 
+            'profile': myProfile})).toBeInstanceOf(HTMLDivElement);
     })
 })
--- UnitImplementations/UIComponents/Dashboard/Dashboard.ts
+++ UnitImplementations/UIComponents/Dashboard/Dashboard.ts
@@ -1,8 +1,20 @@
 import dashboard from '../../../Src/UIComponents/Dashboard/Dashboard'
 let overview = document.createElement('div');
+overview.innerHTML = "Overview";
 let myDiaryFood = document.createElement('div');
+myDiaryFood.innerHTML = "Food";
 let myDiaryExercises = document.createElement('div');
+myDiaryExercises.innerHTML = "Exercises";
 let myGoals = document.createElement('div');
+myGoals.innerHTML = "Goals";
 let myWeights = document.createElement('div');
+myWeights.innerHTML = "Weights";
 let myProfile = document.createElement('div');
-document.body.appendChild(dashboard(overview, myDiaryFood, myDiaryExercises, myGoals, myWeights, myProfile));
+myProfile.innerHTML = "Profile";
+document.body.appendChild(dashboard({
+    'overview': overview, 
+    'diary-food': myDiaryFood, 
+    'diary-exercises': myDiaryExercises, 
+    'goals': myGoals, 
+    'weights': myWeights, 
+    'profile': myProfile}));
