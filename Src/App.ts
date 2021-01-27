5d5d4d09134c4ecf61d749004e2b6151fc49c9b7
JustynaSmoliga <justyna.smoliga@gmail.com>
2021-01-27 10:43:29 +0100

    Tile Component finished. Tests added.

--- Src/App.ts
+++ Src/App.ts
@@ -1,9 +1,2 @@
-import generateTile from './UIComponents/TileComponent/TileComponent';
 console.log("hello");
 
-const content=document.createElement('div');
-content.style.width='277px';
-content.style.height='236px';
-content.style.background='pink';
-content.style.borderRadius='20px';
-document.body.appendChild(generateTile(content));
