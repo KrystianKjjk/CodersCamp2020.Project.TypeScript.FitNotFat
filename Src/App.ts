bfef89567f5a98cf3d2964f8f632732e922c7864
JustynaSmoliga <justyna.smoliga@gmail.com>
2021-01-27 09:58:56 +0100

    create FormComponent ts, css files

--- Src/App.ts
+++ Src/App.ts
@@ -1,1 +1,5 @@
 console.log("hello");
+import generateForm from './UIComponents/FormComponent/FormComponent';
+console.log("hello");
+
+document.body.appendChild(generateForm());
---
+++ Src/UIComponents/FormComponent/FormComponent.ts
@@ -0,0 +1,8 @@
+export default function generateForm() {
+  const myDiv = document.createElement('div');
+  myDiv.style.width = '277px';
+  myDiv.style.height = '236px';
+  myDiv.style.background = 'pink';
+  myDiv.style.borderRadius = '20px';
+  return myDiv;
+}
