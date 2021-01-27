9e619de737461545ae6a18f05075d0e207210043
ireneusz <ireneusz.petryszak@gmail.com>
2021-01-27 12:54:40 +0100

    models: meals changed into map structure

--- Models/DiaryFood.model.ts
+++ Models/DiaryFood.model.ts
@@ -1,11 +1,29 @@
 import {FoodDetails} from "./FoodDetails.model";
 
-export interface DiaryFood {
+type MealMap = {
+    [key: string]: FoodDetails[];
+}
+
+export interface DiaryFoodModel {
     date: Date,
     recommendedKcal: number,
     providedKcal: number,
-    breakfast?: FoodDetails [],
-    lunch?: FoodDetails [],
-    dinner?: FoodDetails [],
-    snacks?: FoodDetails []
+    meals?: MealMap
+}
+
+export class DiaryFood implements DiaryFoodModel {
+    date: Date;
+    recommendedKcal: number;
+    providedKcal: number;
+    meals: MealMap;
+
+    constructor(data: DiaryFoodModel) {
+        Object.assign(this, data);
+        this.meals = data.meals || {};
+    }
+
+    addMeal(key: string, item: FoodDetails) {
+        this.meals[key] = [ ...(this.meals[key] ?? []), item];
+        return this;
+    }
 }
