6cf171d42bf4413fe82ce7dfbddd5accd9623a91
Konrad <kj.reczek@gmail.com>
2021-01-29 20:27:09 +0100

    Change utils function

--- Src/UIComponents/utils/utils.ts
+++ Src/UIComponents/utils/utils.ts
@@ -1,6 +1,6 @@
-export const createElement = (type: string, className: string) => {
+export const createElement = (type: string, className: string = '') => {
     const createdElement = document.createElement(type);
-    createdElement.className = className;
+    className && (createdElement.className = className);
 
     return createdElement;
 }
