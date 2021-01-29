3f0912b73ceb3a80de72a2bb52bdb91cafad6d06
Konrad <kj.reczek@gmail.com>
2021-01-29 21:27:51 +0100

    Create tests for select components

--- Test/Select.test.ts
+++ Test/Select.test.ts
@@ -1,17 +1,35 @@
-import selectOption from '../Src/UIComponents/Select/Select';
+import createSelectBox from '../Src/UIComponents/Select/Select';
 
 describe('Select test', () => {
-    test('', () => {
+    const options = [{key: 'first', label: 'First goal'}];
+    const myFunction = jest.fn();
 
+    test('returns div container', () => {
+        const select = createSelectBox(options, myFunction, 'Questions?')
+        expect(select).toBeInstanceOf(HTMLDivElement);
     })
 
-    test('', () => {
-
+    test('populates options', () => {
+        const select = createSelectBox(options, myFunction, 'Questions?')
+        expect(select.querySelectorAll('.option')).toHaveLength(1);
+        
+        const option: HTMLDivElement = select.querySelector('.option');
+        expect(option.dataset.key).toBe('first');
+        expect(option.innerHTML).toBe('First goal');
     })
 
-    test('', () => {
-
-    })
+    test('clicking an option fires action and adds color to selected option', () => {
+        const optionSelect = [
+            {key: 'first', label: 'First goal'},
+            {key: 'second', label: 'Second goal'},
+            {key: 'third', label: 'Third goal'},
+        ];
+        const selectGroup = createSelectBox(optionSelect, myFunction, 'Why?');
+        const optionFirst: HTMLDivElement = selectGroup.querySelector('.option');
+        optionFirst.click();
+        expect(myFunction).toHaveBeenCalledWith('first');
+        expect(optionFirst.classList.contains('selected')).toBeTruthy();
+    });
 
 })
 
