d01647f40716b03d88156e794fdf0c876864677e
Robert <robert.czwartosz@tlen.pl>
2021-01-29 23:00:37 +0100

    add unit implementation

---
+++ UnitImplementations/UIComponents/AddDiaryItem/AddDiaryItem.ts
@@ -0,0 +1,13 @@
+import addDiaryItem from '../../../Src/UIComponents/AddDiaryItem/AddDiaryItem';
+import { createTable, addRow } from '../../../Src/UIComponents/ReusableTable/ReusableTable';
+
+const placeholder = 'ex. ran 3 miles or 30 min yoga';
+const table = createTable(['Exercise name', 'Met', 'Duration', 'Calories']);
+const addNewRow = addRow(table);
+addNewRow(['running', '9.8', '60 min', '686 kcal']);
+const findCallback = (text: string) => {console.log(text);};
+const addCallback = (table: HTMLElement) => {console.log(table);};
+const cancelCallback = () => {console.log("CANCEL");};
+
+document.body.appendChild(document.createTextNode('Hello'));
+document.body.appendChild(addDiaryItem(table, findCallback, addCallback, cancelCallback, placeholder));
