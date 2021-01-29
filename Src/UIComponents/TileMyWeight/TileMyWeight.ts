602af1dd4f448effd800ce8025cb0ef003836cbb
ireneusz <ireneusz.petryszak@gmail.com>
2021-01-29 22:33:01 +0100

    feature(TileMyWeight): refactoring

--- Src/UIComponents/TileMyWeight/TileMyWeight.ts
+++ Src/UIComponents/TileMyWeight/TileMyWeight.ts
@@ -1,119 +1,113 @@
 import { generateWhiteButton } from '../Buttons/Buttons';
 
-const MONTH_DIFFERENCE = 1;
+const MONTH_DIFFERENCE: number = 1;
+const EXAMPLE_PLACEHOLDER_WEIGHT: string = '61.5';
+const MIN_WEIGHT: number = 1;
+const MAX_WEIGHT: number = 150;
 
 export function createTileMyWeight(currentWeight: number, date: Date, callback: (newWeight: number, newDate: Date) => void): HTMLElement {
 
-  const myWeightTile = document.createElement('div') as HTMLElement;
-  myWeightTile.classList.add('my-weight-tile');
+  const myWeightTileContainer = _createElement('div','my-weight-tile');
+  const dataSection = _createElement('section','my-weight-tile__data-section');
+  const dataSectionHeader = _createElement('p','my-weight-tile__data-section--header', 'MY WEIGHT');
+  const buttonSection = _createElement('section','my-weight-tile__button-section');
 
-  const dataSection = document.createElement('section') as HTMLElement;
-  dataSection.classList.add('my-weight-tile__data-section');
+  dataSection.appendChild(dataSectionHeader);
 
-  const buttonSection = document.createElement('section') as HTMLElement;
-  buttonSection.classList.add('my-weight-tile__button-section');
-
-  const headerDataSection = document.createElement('p') as HTMLElement;
-  headerDataSection.classList.add('my-weight-tile__data-section--header');
-  headerDataSection.innerText = 'MY WEIGHT';
-
-  dataSection.appendChild(headerDataSection);
+  myWeightTileContainer.appendChild(dataSection);
+  myWeightTileContainer.appendChild(buttonSection);
 
   _generateStepOne(dataSection, buttonSection, currentWeight, date, callback);
 
-  myWeightTile.appendChild(dataSection);
-  myWeightTile.appendChild(buttonSection);
-
-  return myWeightTile;
+  return myWeightTileContainer;
 }
 
 function _generateStepOne(dataSection: HTMLElement, buttonSection: HTMLElement, currentWeight: number, date: Date, callback: (newWeight: number, newDate: Date) => void) {
 
-  const mainDataSection = document.createElement('p') as HTMLElement;
-  mainDataSection.classList.add('my-weight-tile__data-section--main-text');
-  mainDataSection.innerText = <string><any>(currentWeight) + ' kg'
+  const dataSectionMain = _createElement('p','my-weight-tile__data-section--main-text',`${currentWeight} kg`);
+  const dataSectionFooter = _createElement('p', 'my-weight-tile__data-section--footer',
+    `${date.getDate()}/${date.getMonth() + MONTH_DIFFERENCE}/${date.getFullYear()}`);
 
-  const footerDataSection = document.createElement('p') as HTMLElement;
-  footerDataSection.classList.add('my-weight-tile__data-section--footer');
-  footerDataSection.innerText = `${date.getDate()}/${date.getMonth() + MONTH_DIFFERENCE}/${date.getFullYear()}`;
+  dataSection.appendChild(dataSectionMain);
+  dataSection.appendChild(dataSectionFooter);
 
-  dataSection.appendChild(mainDataSection);
-  dataSection.appendChild(footerDataSection);
-
-  const editButton = generateWhiteButton('EDIT', () => {
-    mainDataSection.remove();
-    footerDataSection.remove();
-    editButton.remove();
-    _generateStepTwo(dataSection, buttonSection, currentWeight, date, callback)}
-    );
+  const editButton = generateWhiteButton('EDIT', handleEditButtonClick);
   editButton.classList.add('my-weight-tile__button-section--tile-btn');
-  buttonSection.appendChild(editButton)
 
-  return [dataSection, buttonSection];
+  buttonSection.appendChild(editButton);
+
+  function handleEditButtonClick() {
+    _removeElements([dataSectionMain, dataSectionFooter, editButton]);
+    _generateStepTwo(dataSection, buttonSection, currentWeight, date, callback);
+  }
+
 }
 
 function _generateStepTwo(dataSection: HTMLElement, buttonSection: HTMLElement, currentWeight: number, oldDate: Date, callback: (newWeight: number, newDate: Date) => void) {
 
   const newDate = new Date();
 
-  const mainDataSectionContainer = document.createElement('div');
-  mainDataSectionContainer.classList.add("my-weight-tile__data-section--container");
+  const dataSectionMainContainer = _createElement('div','my-weight-tile__data-section--container');
+  const dataSectionMainInput = _createElementInput('input','my-weight-tile__data-section--input', 'number', EXAMPLE_PLACEHOLDER_WEIGHT);
+  const dataSectionMainText = _createElement('p','my-weight-tile__data-section--main-text-step-two', ' kg');
+  const dataSectionMainIncorrect = _createElement('p','my-weight-tile__data-section--incorrect', 'Invalid value!');
 
-  const mainDataSectionInput = document.createElement('input') as HTMLInputElement;
-  mainDataSectionInput.classList.add('my-weight-tile__data-section--input');
-  mainDataSectionInput.type = 'number';
-  mainDataSectionInput.placeholder = '61.5'
+  dataSectionMainContainer.appendChild(dataSectionMainInput);
+  dataSectionMainContainer.appendChild(dataSectionMainText);
 
-  const mainDataSectionText = document.createElement('p') as HTMLElement;
-  mainDataSectionText.classList.add('my-weight-tile__data-section--main-text-step-two');
-  mainDataSectionText.innerText = ' kg'
+  const dataSectionFooter = _createElement('p', 'my-weight-tile__data-section--footer',
+    `${newDate.getDate()}/${newDate.getMonth() + MONTH_DIFFERENCE}/${newDate.getFullYear()}` );
 
-  var mainDataSectionIncorrect = document.createElement('p');
-  mainDataSectionIncorrect.classList.add('my-weight-tile__data-section--incorrect');
-  mainDataSectionIncorrect.innerText = 'Invalid value!';
+  dataSection.appendChild(dataSectionMainContainer);
+  dataSection.appendChild(dataSectionFooter);
 
-  mainDataSectionContainer.appendChild(mainDataSectionInput);
-  mainDataSectionContainer.appendChild(mainDataSectionText);
-
-  const footerDataSection = document.createElement('p') as HTMLElement;
-  footerDataSection.classList.add('my-weight-tile__data-section--footer');
-  footerDataSection.innerText = `${newDate.getDate()}/${newDate.getMonth() + MONTH_DIFFERENCE}/${newDate.getFullYear()}`;
-
-
-  dataSection.appendChild(mainDataSectionContainer);
-  dataSection.appendChild(footerDataSection);
-
-  const saveButton = generateWhiteButton('SAVE', ()=>{
-    const newWeight: number = <number><any>mainDataSectionInput.value;
-    if(newWeight<=0 || newWeight > 150) {
-        if(!document.querySelector('.my-weight-tile__data-section--incorrect')) {
-          mainDataSectionContainer.parentNode.insertBefore(mainDataSectionIncorrect,mainDataSectionContainer.nextElementSibling);
-        }
-      return;
-    }
-    callback(newWeight, newDate);
-    mainDataSectionContainer.remove();
-    mainDataSectionIncorrect.remove();
-    footerDataSection.remove();
-    saveButton.remove();
-    cancelButton.remove();
-
-    _generateStepOne(dataSection, buttonSection, newWeight, newDate, callback)
-  });
+  const saveButton = generateWhiteButton('SAVE', handleSaveButtonClick);
   saveButton.classList.add('my-weight-tile__button-section--tile-btn');
 
-  const cancelButton = generateWhiteButton('CANCEL', ()=>{
-    mainDataSectionContainer.remove();
-    footerDataSection.remove();
-    mainDataSectionIncorrect.remove();
-    saveButton.remove();
-    cancelButton.remove();
-    _generateStepOne(dataSection, buttonSection, currentWeight, oldDate, callback)
-  });
+  const cancelButton = generateWhiteButton('CANCEL', handleCancelButtonClick);
   cancelButton.classList.add('my-weight-tile__button-section--tile-btn');
+
+  const elementsRemove = [dataSectionMainContainer, dataSectionMainIncorrect, dataSectionFooter, saveButton, cancelButton];
 
   buttonSection.appendChild(saveButton)
   buttonSection.appendChild(cancelButton)
 
-  return [dataSection, buttonSection];
+
+  function handleSaveButtonClick() {
+    const newWeight: number = <number><any>dataSectionMainInput.value;
+
+    if(newWeight <= MIN_WEIGHT || newWeight > MAX_WEIGHT) {
+      if(!document.querySelector('.my-weight-tile__data-section--incorrect')) {
+        dataSectionMainContainer.parentNode.insertBefore(dataSectionMainIncorrect,dataSectionMainContainer.nextElementSibling);
+      }
+      return;
+    }
+
+    callback(newWeight, newDate);
+    _removeElements(elementsRemove);
+    _generateStepOne(dataSection, buttonSection, newWeight, newDate, callback)
+  }
+
+  function handleCancelButtonClick() {
+    _removeElements(elementsRemove);
+    _generateStepOne(dataSection, buttonSection, currentWeight, oldDate, callback)
+  }
+}
+
+function _createElement(name: string, className: string, innerText?: string): HTMLElement {
+  const element = document.createElement(name) as HTMLElement;
+  element.classList.add(className);
+  if(innerText) element.innerText = innerText;
+  return element;
+}
+
+function _createElementInput(name: string, className: string, type: string, placeholder: string): HTMLInputElement {
+  const element = _createElement(name, className) as HTMLInputElement;
+  if(type) element.type = type;
+  if(placeholder) element.placeholder = placeholder;
+  return element;
+}
+
+function _removeElements(elements: HTMLElement[]): void{
+  elements.forEach( element => element.remove());
 }
