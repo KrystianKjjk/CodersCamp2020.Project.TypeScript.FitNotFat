b725e8575f64489e7ac71e86675b6ac8fb872a04
JustynaSmoliga <justyna.smoliga@gmail.com>
2021-01-29 15:07:05 +0100

    Code divided into functions

--- Src/UIComponents/FormComponent/FormComponent.ts
+++ Src/UIComponents/FormComponent/FormComponent.ts
@@ -1,124 +1,149 @@
-import {generateRedButton} from '../Buttons/Buttons';
+import { generateRedButton } from '../Buttons/Buttons';
+
 
 export default function generateForm() {
- const form=document.createElement('form');
+  const form = document.createElement('form');
+  
+  //paragraf
+  const paragraph = generateParagraph();
+  form.appendChild(paragraph);
 
- //paragraf
- const paragraph=document.createElement('p');
- const node=document.createTextNode('Join us');
- paragraph.appendChild(node);
- form.appendChild(paragraph);
+  //progress-bar
+  const progressBar=generateProgressBar();
+  form.appendChild(progressBar);
 
- //progress-bar
-const progressBar=document.createElement('div');
-progressBar.className="progress-bar";
-form.appendChild(progressBar);
-const stepDiv=document.createElement('div');
-stepDiv.className="first-step";
-progressBar.appendChild(stepDiv);
+  //name input
+  const nameInput = generateTextInput('Nickname', 'name');
+  nameInput.setAttribute('minlength', '4');
+  form.appendChild(nameInput);
 
-//name input
-const nameInput=document.createElement('input');
-nameInput.setAttribute('type','text');
-nameInput.setAttribute('placeholder','Nickname');
-nameInput.setAttribute('minlength','4');
-nameInput.required=true;
-nameInput.className='name';
-form.appendChild(nameInput);
+  //gender-selection
+  const genderSelectionDiv = generateGenderSelectionDiv();
+  form.appendChild(genderSelectionDiv);
 
-//gender-selection
-const genderSelectionDiv=document.createElement('div');
-genderSelectionDiv.className='gender-selection';
-form.appendChild(genderSelectionDiv);
+  //input birth date
+  const dateInput = generateDateInput();
+  form.appendChild(dateInput);
 
-//input and label male 
-const maleInput=document.createElement('input');
-maleInput.setAttribute('type','radio');
-maleInput.setAttribute('name','gender');
-maleInput.setAttribute('value','male');
-maleInput.setAttribute('id','male');
-maleInput.required=true;
+  //current values div
+  const heightAndWeightDiv = document.createElement('div');
+  heightAndWeightDiv.className = 'current-values';
+  form.appendChild(heightAndWeightDiv);
 
-const maleLabel=document.createElement('label');
-maleLabel.setAttribute('for','male'); 
-genderSelectionDiv.appendChild(maleInput);
-genderSelectionDiv.appendChild(maleLabel);
-maleLabel.innerHTML='Male'; 
-const genderSelectionSpan=document.createElement('span');
-genderSelectionDiv.appendChild(genderSelectionSpan);
+  //height
+  const heightInput = generateNumberInput('Height (cm)', 'height');
+  heightAndWeightDiv.appendChild(heightInput);
 
-// input and label female
-const femaleInput=document.createElement('input');
-femaleInput.setAttribute('type','radio');
-femaleInput.setAttribute('name','gender');
-femaleInput.setAttribute('value','female');
-femaleInput.setAttribute('id','female');
-femaleInput.required=true;
+  //weight
+  const weightInput = generateNumberInput(
+    'Current weight (kg)',
+    'current-weight',
+  );
+  heightAndWeightDiv.appendChild(weightInput);
 
-const femaleLabel=document.createElement('label');
-femaleLabel.setAttribute('for','female'); 
-genderSelectionDiv.appendChild(femaleInput);
-genderSelectionDiv.appendChild(femaleLabel);
-femaleLabel.innerHTML='Female';
+  //goal
+  const goalWeightInput = generateNumberInput(
+    'Goal weight (kg)',
+    'goal-weight',
+  );
+  form.appendChild(goalWeightInput);
 
-//input birth date
-const dateInput=document.createElement('input');
-dateInput.setAttribute('type','text');
-dateInput.setAttribute('placeholder','Date of birth');
-dateInput.addEventListener('focus', (event)=>{
-  dateInput.setAttribute('type','date');
-})
-dateInput.addEventListener('blur', (event)=>{
-  dateInput.setAttribute('type','text');
-})
-dateInput.required=true;
-dateInput.className='birth-date';
-form.appendChild(dateInput);
+  //button
+  const submitButton = generateRedButton('Next step', fn);
+  submitButton.style.height = '50px';
+  submitButton.style.width = '35%';
+  form.appendChild(submitButton);
 
-//current values div
-const heightAndWeightDiv=document.createElement('div');
-heightAndWeightDiv.className='current-values';
-form.appendChild(heightAndWeightDiv);
+  function fn() {
+    console.log('test');
+  }
+  return form;
 
-//height input (to i current weight są prawie identyczne)
-const heightInput=document.createElement('input');
-heightInput.setAttribute('type','number');
-heightInput.setAttribute('placeholder','Height (cm)');
-heightInput.setAttribute('min','0');
-heightInput.required=true;
-heightInput.className='height';
-heightAndWeightDiv.appendChild(heightInput);
-
-//current weight input
-const weightInput=document.createElement('input');
-weightInput.setAttribute('type','number');
-weightInput.setAttribute('placeholder','Current weight (kg)');
-weightInput.setAttribute('min','0');
-weightInput.required=true;
-weightInput.className='current-weight';
-heightAndWeightDiv.appendChild(weightInput);
-
-//goal weight input
-const goalWeightInput=document.createElement('input');
-goalWeightInput.setAttribute('type','number');
-goalWeightInput.setAttribute('placeholder','Goal weight (kg)');
-goalWeightInput.setAttribute('min','0');
-goalWeightInput.required=true;
-goalWeightInput.className='goal-weight';
-form.appendChild(goalWeightInput);
-
-//button 
-const submitButton=generateRedButton('Next step', fn);
-submitButton.style.height='50px';
-submitButton.style.width='35%';
-form.appendChild(submitButton);
-
-function fn(){
-  console.log('test');
-}
-return form;
-
-//można zrobić funkcję required, która do każdego inputa dodaje
-//required=true; 
 }
 
+ //FUNCTIONS
+ function generateParagraph():HTMLParagraphElement{
+  const paragraph = document.createElement('p');
+  const node = document.createTextNode('Join us');
+  paragraph.appendChild(node);
+  return paragraph;
+  }
+
+ function generateProgressBar():HTMLDivElement {
+  const progressBar = document.createElement('div');
+  progressBar.className = 'progress-bar';
+  const stepDiv = document.createElement('div');
+  stepDiv.className = 'first-step';
+  progressBar.appendChild(stepDiv);
+  return progressBar;
+}
+
+function generateTextInput(placeholderValue: string, className: string):HTMLInputElement {
+  const textInput = document.createElement('input');
+  textInput.setAttribute('type', 'text');
+  textInput.setAttribute('placeholder', placeholderValue);
+  textInput.required = true;
+  textInput.className = className;
+  return textInput;
+}
+
+function generateGenderSelectionDiv():HTMLDivElement {
+  const genderSelectionDiv = document.createElement('div');
+  genderSelectionDiv.className = 'gender-selection';
+
+  const maleInput = generateRadioInput('male');
+  const maleLabel = generateLabel('male');
+  genderSelectionDiv.appendChild(maleInput);
+  genderSelectionDiv.appendChild(maleLabel);
+  maleLabel.innerHTML = 'Male';
+
+  const genderSelectionSpan = document.createElement('span');
+  genderSelectionDiv.appendChild(genderSelectionSpan);
+
+  const femaleInput = generateRadioInput('female');
+  const femaleLabel = generateLabel('female');
+  genderSelectionDiv.appendChild(femaleInput);
+  genderSelectionDiv.appendChild(femaleLabel);
+  femaleLabel.innerHTML = 'Female';
+  return genderSelectionDiv;
+}
+
+function generateRadioInput(inputValue: string): HTMLInputElement {
+  const radioInput = document.createElement('input');
+  radioInput.setAttribute('type', 'radio');
+  radioInput.setAttribute('name', 'gender');
+  radioInput.setAttribute('value', inputValue);
+  radioInput.setAttribute('id', inputValue);
+  radioInput.required = true;
+  return radioInput;
+}
+
+function generateLabel(id: string):HTMLLabelElement {
+  const label = document.createElement('label');
+  label.setAttribute('for', id);
+  return label;
+}
+
+function generateDateInput():HTMLInputElement{
+  const dateInput = generateTextInput('Date of birth', 'birth-date');
+  dateInput.addEventListener('focus', () => {
+    dateInput.setAttribute('type', 'date');
+  });
+  dateInput.addEventListener('blur', () => {
+    dateInput.setAttribute('type', 'text');
+  });
+  return dateInput;
+}
+
+function generateNumberInput(
+  placeholderValue: string,
+  className: string,
+): HTMLInputElement {
+  const numberInput = document.createElement('input');
+  numberInput.setAttribute('type', 'number');
+  numberInput.setAttribute('placeholder', placeholderValue);
+  numberInput.setAttribute('min', '0');
+  numberInput.required = true;
+  numberInput.className = className;
+  return numberInput;
+}
