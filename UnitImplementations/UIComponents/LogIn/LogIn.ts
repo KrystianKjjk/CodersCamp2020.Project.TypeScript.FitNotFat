aee0cd64be739456ed807e4d41774ab77f75485e
Konrad <kj.reczek@gmail.com>
2021-02-01 18:07:30 +0100

    Improve login view

--- UnitImplementations/UIComponents/LogIn/LogIn.ts
+++ UnitImplementations/UIComponents/LogIn/LogIn.ts
@@ -1,5 +1,5 @@
 import {generateWhiteButton, generateRedButton} from '../../../Src/UIComponents/Buttons/Buttons';
-
+import {generateTextInput} from '../../../Src/UIComponents/FormComponent/FormComponent'
 
 const container = document.createElement('div');
 container.className = 'main-container';
@@ -9,6 +9,9 @@ title.appendChild(document.createTextNode('Log In'));
 
 container.appendChild(title);
 document.body.appendChild(container);
+
+const input = generateTextInput('Nickname', 'name');
+container.appendChild(input);
 
 const button = generateRedButton('Log In', ()=>{});
 
