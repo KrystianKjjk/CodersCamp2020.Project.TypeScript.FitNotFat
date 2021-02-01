55843258be32c33d5d1e824962ee42daa0cf76e7
Robert <robert.czwartosz@tlen.pl>
2021-02-01 21:43:10 +0100

    refactor Buttons.ts

--- Src/UIComponents/Buttons/Buttons.ts
+++ Src/UIComponents/Buttons/Buttons.ts
@@ -1,19 +1,14 @@
-function generateButton(label:string, onClick:Function, infill:boolean) :HTMLButtonElement {
-   
-    const button:HTMLButtonElement = document.createElement('button');
-    button.className = 'button';
-    button.innerText = label;
+import { createElement } from '../utils/utils';
 
-    button.style.backgroundColor = infill ? "#DA1B36" : "white";       
+function generateButton(label:string, onClick:Function, infill:boolean): HTMLButtonElement {
+    const button = createElement('button', 'button', label) as HTMLButtonElement;
+    button.style.backgroundColor = infill ? "#DA1B36" : "white";
     button.style.color = infill ? "white": "#DA1B36";  
-
     button.addEventListener('click', (e) => {
         onClick();
     }); 
-
     return button;
   }
-
 
   function generateRedButton(label:string , onClick:Function):HTMLButtonElement {
     return generateButton(label , onClick, true);
