3ba683d1dd70aa5abba0def36a665022420eeafe
Robert <robert.czwartosz@tlen.pl>
2021-02-02 20:34:05 +0100

    develop unit implementation

--- UnitImplementations/UIComponents/LogIn/LogIn.ts
+++ UnitImplementations/UIComponents/LogIn/LogIn.ts
@@ -1,6 +1,21 @@
 import {generateWhiteButton, generateRedButton} from '../../../Src/UIComponents/Buttons/Buttons';
-import {generateTextInput} from '../../../Src/UIComponents/FormComponent/FormComponent'
+import {generateTextInput} from '../../../Src/UIComponents/FormComponent/FormComponent';
 import {createElement} from '../../../Src/UIComponents/utils/utils'
+import loginButton from '../../../Src/Logic/LoginButton/LoginButton';
+import {saveInLocalStorage} from '../../../Src/Logic/LocalStorage/LocalStorage';
+import {ActivityLevel} from '../../../Models/ActivityLevel.model';
+import {User} from '../../../Models/User.model';
+
+const username = 'User3';
+const user: User = {
+  name: username,
+  gender: 'Male',
+  dateOfBirth: new Date(),
+  height: 177,
+  activityLevel: ActivityLevel.Low,
+  goalWeight: 70,  
+};
+saveInLocalStorage(username, user);
 
 const container = createElement('div', 'main-container');
 const title = createElement('p', 'main-title'); 
@@ -12,7 +27,10 @@ document.body.appendChild(container);
 const input = generateTextInput('Nickname', 'name');
 container.appendChild(input);
 
-const button = generateRedButton('Log In', ()=>{});
+const failMsg = createElement('p');
+failMsg.innerHTML = "Username does not exist";
+let button: HTMLButtonElement;
+button = generateRedButton('Log In', () => loginButton.call(button, input.value, generateWhiteButton('Hello '+input.value, () => {}), failMsg));
 
 const containerButton = createElement('div', 'container-button'); 
 containerButton.appendChild(button);
