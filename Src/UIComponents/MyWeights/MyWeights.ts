6da1579c3ad35665f8c7fe896883bb55d3a16071
FIlip <filip.szwedo@gmail.com>
2021-02-03 08:15:05 +0100

    Code Review changes

--- Src/UIComponents/MyWeights/MyWeights.ts
+++ Src/UIComponents/MyWeights/MyWeights.ts
@@ -5,10 +5,7 @@ import { User } from '../../../Models/User.model';
 export default function myWeightsComponent(user:User):HTMLDivElement{
     const container = createElement('div', 'my-weights-container') as HTMLDivElement;
     const weightsTile = generateWeightTable(user.weights);
-    const header = createElement('div', 'myweights-header');
-    const headerText = document.createTextNode('My weights');
-
-    header.append(headerText);
+    const header = createElement('div', 'myweights-header', 'My Weights');
 
     container.append(header, weightsTile);
     return container;
