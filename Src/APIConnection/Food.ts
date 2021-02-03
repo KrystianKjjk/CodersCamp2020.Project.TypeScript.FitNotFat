6f74bba84862d219fdde8ad2f3e31cfa31d1f911
Konrad <kj.reczek@gmail.com>
2021-02-03 19:25:09 +0100

    Add api connection functions

--- Src/APIConnection/Food.ts
+++ Src/APIConnection/Food.ts
@@ -1,6 +1,16 @@
-async function getFood(food: string) {
+export interface FoodItemData {
+    food_name: string;
+    serving_qty: number;
+    serving_unit: string;
+    nf_calories: number;
+}
+
+export interface FoodDataFromResponse {
+    foods: FoodItemData[];
+}
+
+async function fetchFoodData(food: string): Promise<FoodDataFromResponse> {
     const url = "https://trackapi.nutritionix.com/v2/natural/nutrients";
-    const MAX_RESULTS = 3;
     const requestJSON = {"query": food}
     
     const response = await fetch(url, {
@@ -9,11 +19,23 @@ async function getFood(food: string) {
         headers: {
             'accept': 'application/json',
             'Content-Type': 'application/json',
-            'x-app-id': 'f3324dd3',
-            'x-app-key': '7fa425ed94ca1fe1a1e5541a16ef8885'
+            'x-app-id': 'X',
+            'x-app-key': 'C'
           },
     });
     
     return response.json();
 }
-export { getFood };
+
+async function foodData(food: string, callback: (data: string[]) => void) {
+    const apiFood = await fetchFoodData(food);
+    console.log(apiFood);
+    return apiFood.foods.forEach(foodItem => callback([
+        foodItem.food_name, 
+        ''+foodItem.serving_qty, 
+        foodItem.serving_unit, 
+        foodItem.nf_calories+' kcal'
+    ]));
+}
+
+export { fetchFoodData, foodData };
