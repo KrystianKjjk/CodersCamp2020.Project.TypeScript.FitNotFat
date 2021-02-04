7c43ca9fa5d60c10584f91b246dc1b36fee41a77
Justyna <justyna.smoliga@gmail.com>
2021-02-05 00:06:56 +0100

    Registration first step

--- Src/Logic/Registration/Registration.ts
+++ Src/Logic/Registration/Registration.ts
@@ -1,16 +1,30 @@
 import generateFirstStepForm from '../../UIComponents/FormComponent/FormComponent';
-import {User} from '../../../Models/User.model';
-import {ActivityLevel} from '../../../Models/ActivityLevel.model';
-export default function generateRegistrationForm(onRegistrationSuccess:Function):HTMLElement {
-   
-    let name: string;
-    let gender: 'Male' | 'Female';
-    let dateOfBirth: Date;
-    let height: number;
-    let currentWeight: number;
-    let goalWeight: number;
-    let activityLevel: ActivityLevel;
-    
-    const firstStepForm=generateFirstStepForm();
-    return firstStepForm;
+import { User } from '../../../Models/User.model';
+import { ActivityLevel } from '../../../Models/ActivityLevel.model';
+import { FirstStepFormValues } from '../../../Src/UIComponents/FormComponent/FormComponent';
+import {createElement} from '../../../Src/UIComponents/utils/utils';
+
+export default function generateRegistrationForm(
+  onRegistrationSuccess: Function,
+): HTMLElement {
+  // let name: string;
+  // let gender: 'Male' | 'Female';
+  // let dateOfBirth: Date;
+  // let height: number;
+  // let currentWeight: number;
+  // let goalWeight: number;
+  // let activityLevel: ActivityLevel;
+const registrationDiv=createElement('div','div-registration');
+
+  const firstStepForm = generateFirstStepForm(onNextStepClick);
+  let firstStepValues;
+  registrationDiv.appendChild(firstStepForm);
+
+  function onNextStepClick(firstStepFormValues: FirstStepFormValues): void {
+    firstStepValues = firstStepFormValues;
+    console.log(firstStepValues);
+  }
+
+
+  return registrationDiv;
 }
--- Src/UIComponents/FormComponent/FormComponent.ts
+++ Src/UIComponents/FormComponent/FormComponent.ts
@@ -1,20 +1,21 @@
 import { generateRedButton } from '../Buttons/Buttons';
-interface FirstStepFormValues{
-   name: string;
-   gender: 'Male' | 'Female';
-   dateOfBirth: Date;
-   height: number;
-   currentWeight: number;
-   goalWeight: number;
+
+export interface FirstStepFormValues {
+  name: string;
+  gender: 'Male' | 'Female';
+  dateOfBirth: Date | string;
+  height: number;
+  currentWeight: number;
+  goalWeight: number;
 }
 
-
-export default function generateForm(onNextStepClick:(firstSTepFormValues:FirstStepFormValues)=>void):HTMLFormElement{
-
+export default function generateForm(
+  onNextStepClick: (firstStepFormValues: FirstStepFormValues) => void,
+): HTMLFormElement {
   const form = document.createElement('form');
 
   const paragraph = generateParagraph();
-  const progressBar=generateProgressBar();
+  const progressBar = generateProgressBar();
 
   const nameInput = generateTextInput('Nickname', 'name');
   nameInput.setAttribute('minlength', '3');
@@ -27,37 +28,58 @@ export default function generateForm(onNextStepClick:(firstSTepFormValues:FirstS
   heightAndWeightDiv.className = 'current-values';
 
   const heightInput = generateNumberInput('Height (cm)', 'height');
-  
 
   const weightInput = generateNumberInput(
     'Current weight (kg)',
     'current-weight',
   );
 
-  heightAndWeightDiv.append(heightInput,weightInput);
+  heightAndWeightDiv.append(heightInput, weightInput);
 
   const goalWeightInput = generateNumberInput(
     'Goal weight (kg)',
     'goal-weight',
   );
 
-  const submitButton = generateRedButton('Next step', ()=>{});
+  const submitButton = generateRedButton('Next step', () => {
+    const gender = form.querySelector(
+      'input[name="gender"]:checked',
+    ) as HTMLInputElement;
+    const formValues: FirstStepFormValues = {
+      name: nameInput.value,
+      gender: gender.value as 'Male' | 'Female',
+      dateOfBirth: new Date(dateInput.value),
+      height: parseInt(heightInput.value),
+      currentWeight: parseInt(weightInput.value),
+      goalWeight: parseInt(goalWeightInput.value),
+    };
+    console.log(formValues.dateOfBirth);
+    onNextStepClick(formValues);
+  });
   submitButton.style.height = '50px';
   submitButton.style.width = '35%';
 
-  form.append(paragraph,progressBar,nameInput, genderSelectionDiv, dateInput, heightAndWeightDiv,goalWeightInput, submitButton);
+  form.append(
+    paragraph,
+    progressBar,
+    nameInput,
+    genderSelectionDiv,
+    dateInput,
+    heightAndWeightDiv,
+    goalWeightInput,
+    submitButton,
+  );
   return form;
 }
 
- 
- function generateParagraph():HTMLParagraphElement{
+function generateParagraph(): HTMLParagraphElement {
   const paragraph = document.createElement('p');
   const node = document.createTextNode('Join us');
   paragraph.appendChild(node);
   return paragraph;
-  }
+}
 
- function generateProgressBar():HTMLDivElement {
+function generateProgressBar(): HTMLDivElement {
   const progressBar = document.createElement('div');
   progressBar.className = 'progress-bar';
   const stepDiv = document.createElement('div');
@@ -66,7 +88,10 @@ export default function generateForm(onNextStepClick:(firstSTepFormValues:FirstS
   return progressBar;
 }
 
-function generateTextInput(placeholderValue: string, className: string):HTMLInputElement {
+function generateTextInput(
+  placeholderValue: string,
+  className: string,
+): HTMLInputElement {
   const textInput = document.createElement('input');
   textInput.setAttribute('type', 'text');
   textInput.setAttribute('placeholder', placeholderValue);
@@ -75,21 +100,21 @@ function generateTextInput(placeholderValue: string, className: string):HTMLInpu
   return textInput;
 }
 
-function generateGenderSelectionDiv():HTMLDivElement {
+function generateGenderSelectionDiv(): HTMLDivElement {
   const genderSelectionDiv = document.createElement('div');
   genderSelectionDiv.className = 'gender-selection';
 
-  const maleInput = generateRadioInput('male');
-  const maleLabel = generateLabel('male');
+  const maleInput = generateRadioInput('Male');
+  const maleLabel = generateLabel('Male');
   genderSelectionDiv.append(maleInput, maleLabel);
   maleLabel.innerHTML = 'Male';
 
   const genderSelectionSpan = document.createElement('span');
   genderSelectionDiv.appendChild(genderSelectionSpan);
 
-  const femaleInput = generateRadioInput('female');
-  const femaleLabel = generateLabel('female');
-  genderSelectionDiv.append(femaleInput,femaleLabel);
+  const femaleInput = generateRadioInput('Female');
+  const femaleLabel = generateLabel('Female');
+  genderSelectionDiv.append(femaleInput, femaleLabel);
   femaleLabel.innerHTML = 'Female';
   return genderSelectionDiv;
 }
@@ -104,13 +129,13 @@ function generateRadioInput(inputValue: string): HTMLInputElement {
   return radioInput;
 }
 
-function generateLabel(id: string):HTMLLabelElement {
+function generateLabel(id: string): HTMLLabelElement {
   const label = document.createElement('label');
   label.setAttribute('for', id);
   return label;
 }
 
-function generateDateInput():HTMLInputElement{
+function generateDateInput(): HTMLInputElement {
   const dateInput = generateTextInput('Date of birth', 'birth-date');
   dateInput.addEventListener('focus', () => {
     dateInput.setAttribute('type', 'date');
@@ -134,4 +159,4 @@ function generateNumberInput(
   return numberInput;
 }
 
-export {generateTextInput};
+export { generateTextInput };
