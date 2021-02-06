f573a3dcd7beb8a9bacd1a394fd3bc1d19309787
Justyna <justyna.smoliga@gmail.com>
2021-02-06 13:15:53 +0100

    user creation completed

--- Src/Logic/Registration/Registration.ts
+++ Src/Logic/Registration/Registration.ts
@@ -3,30 +3,40 @@ import { User } from '../../../Models/User.model';
 import { ActivityLevel } from '../../../Models/ActivityLevel.model';
 import { FirstStepFormValues } from '../../../Src/UIComponents/FormComponent/FormComponent';
 import {createElement} from '../../../Src/UIComponents/utils/utils';
+import {SecondStepValues,generateSecondStep} from '../../../Src/UIComponents/SecondStepComponent/SecondStepComponent';
+import { Goal } from '../../../Models/Goal.model';
+import { Weight } from '../../../Models/Weight.model';
 
 export default function generateRegistrationForm(
   onRegistrationSuccess: Function,
 ): HTMLElement {
-  // let name: string;
-  // let gender: 'Male' | 'Female';
-  // let dateOfBirth: Date;
-  // let height: number;
-  // let currentWeight: number;
-  // let goalWeight: number;
-  // let activityLevel: ActivityLevel;
 const registrationDiv=createElement('div','div-registration');
 
 let firstStepValues;
+const firstStepForm = generateFirstStepForm(onNextStepClick);
+registrationDiv.appendChild(firstStepForm);
+const secondStepForm=generateSecondStep(onBackClick,onSignUpClick);
+
 function onNextStepClick(firstStepFormValues: FirstStepFormValues): void {
   firstStepValues = firstStepFormValues;
-  console.log(firstStepValues);
+  registrationDiv.replaceChild(secondStepForm,firstStepForm);
 }
-  const firstStepForm = generateFirstStepForm(onNextStepClick);
-  
-  registrationDiv.appendChild(firstStepForm);
 
- 
+function onBackClick() {
+  registrationDiv.replaceChild(firstStepForm,secondStepForm);
+}
 
-
+function onSignUpClick(secondStepValues:SecondStepValues){
+const weight:Weight={date:new Date(), weight:firstStepValues.currentWeight};
+  const user:User={
+  name: firstStepValues.name,
+  gender: firstStepValues.gender,
+  dateOfBirth: firstStepValues.dateOfBirth,
+  height: firstStepValues.height,
+  goalWeight: firstStepValues.goalWeight,
+  activityLevel: secondStepValues.activityLevel,
+  weights:[weight],
+}
+}
   return registrationDiv;
 }
--- Src/UIComponents/SecondStepComponent/SecondStepComponent.ts
+++ Src/UIComponents/SecondStepComponent/SecondStepComponent.ts
@@ -1,68 +1,85 @@
-import {createRadioGroup} from '../../../Src/UIComponents/RadioGroup/RadioGroup';
+import { createRadioGroup } from '../../../Src/UIComponents/RadioGroup/RadioGroup';
 import createSelectBox from '../../../Src/UIComponents/Select/Select';
-import {generateRedButton, generateWhiteButton}from '../../../Src/UIComponents/Buttons/Buttons';
-import {createElement} from '../../../Src/UIComponents/utils/utils';
-import {WeightGoal,createWeightGoalSelect} from '../../../Src/UIComponents/WeeklyGoalComponent/WeeklyGoalComponent';
+import {
+  generateRedButton,
+  generateWhiteButton,
+} from '../../../Src/UIComponents/Buttons/Buttons';
+import { createElement } from '../../../Src/UIComponents/utils/utils';
+import {
+  WeightGoal,
+  createWeightGoalSelect,
+} from '../../../Src/UIComponents/WeeklyGoalComponent/WeeklyGoalComponent';
+import { ActivityLevel } from '../../../Models/ActivityLevel.model';
 
-export function generateSecondStep(){
-const secondStepDiv=createElement('div','main-container');
-const title=createElement('p','main-title');
-title.appendChild(document.createTextNode('Join us'));
-const line=createElement('hr','main-hr');
-
-let selectedValue;
-
-function onWeightGoalSelect(selectedVal:WeightGoal){
-selectedValue=selectedVal;
+export interface SecondStepValues{
+    weeklyGoal:WeightGoal;
+    activityLevel:ActivityLevel;
 }
 
-const select=createWeightGoalSelect(onWeightGoalSelect);
+export function generateSecondStep(onBackClick:()=>void,onSignUpClick:(secondStepValues:SecondStepValues)=>void) {
+  const secondStepDiv = createElement('div', 'main-container');
+  const title = createElement('p', 'main-title');
+  title.appendChild(document.createTextNode('Join us'));
+  const line = createElement('hr', 'main-hr');
 
-const optionsRadio = [{
-    key: "sitting",
-    name: "active",
-    label: "Spend most of the day sitting",
-},
-{
-    key: "onfeet",
-    name: "active",
-    label: "Spend significant part of the day on your feet",
-},
-{
-    key: "ssd",
-    name: "active",
-    label: "Spend significant part of the day doing physical activities",
-},
-{
-    key: "osds",
-    name: "active",
-    label: "Spend most of the day doing heavy physical activities",
-}]
+  let selectedValue;
+  let radioSelectedValue;
 
-const functionRadio = (name: string) => alert(name);
+  function onWeightGoalSelect(selectedVal: WeightGoal) {
+    selectedValue = selectedVal;
+  }
 
-const radio=createRadioGroup(optionsRadio, functionRadio, "How active are you?");
+  const select = createWeightGoalSelect(onWeightGoalSelect);
 
-const button1 = generateWhiteButton('Back', ()=>{});
-const button2 = generateRedButton('Sign Up', ()=>{});
+  const optionsRadio = [
+    {
+      key: ActivityLevel.Low,
+      name: 'active',
+      label: 'Spend most of the day sitting',
+    },
+    {
+      key: ActivityLevel.Regular,
+      name: 'active',
+      label: 'Spend significant part of the day on your feet',
+    },
+    {
+      key: ActivityLevel.Active,
+      name: 'active',
+      label: 'Spend significant part of the day doing physical activities',
+    },
+    {
+      key: ActivityLevel.VeryActive,
+      name: 'active',
+      label: 'Spend most of the day doing heavy physical activities',
+    },
+  ];
 
-button1.style.width = "200px";
-button2.style.width = "200px";
+  function onRadioSelect(radioOptionKey: string) {
+    radioSelectedValue = radioOptionKey;
+  }
 
-button1.style.height = "50px";
-button2.style.height = "50px";
+  const radio = createRadioGroup(
+    optionsRadio,
+    onRadioSelect,
+    'How active are you?',
+  );
 
-const containerButton = document.createElement('div');
-containerButton.className = 'container-button';
-containerButton.append(button1,button2);
+  const button1 = generateWhiteButton('Back', () => {onBackClick()});
+  const button2 = generateRedButton('Sign Up', () => {
+    const secondStepValues:SecondStepValues={weeklyGoal:selectedValue, activityLevel:radioSelectedValue}  
+    onSignUpClick(secondStepValues)});
 
+  button1.style.width = '200px';
+  button2.style.width = '200px';
 
-secondStepDiv.append(title,line,select,radio,containerButton);
+  button1.style.height = '50px';
+  button2.style.height = '50px';
 
+  const containerButton = document.createElement('div');
+  containerButton.className = 'container-button';
+  containerButton.append(button1, button2);
 
-return secondStepDiv;
+  secondStepDiv.append(title, line, select, radio, containerButton);
+
+  return secondStepDiv;
 }
-
-    
-
-
