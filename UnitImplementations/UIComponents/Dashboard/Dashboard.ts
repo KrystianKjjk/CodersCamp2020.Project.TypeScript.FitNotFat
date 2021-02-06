3c8aeade00ac53389d6c2338f27fe383a74dff03
Robert <robert.czwartosz@tlen.pl>
2021-02-06 20:34:32 +0100

    apply compo in unit implementations

--- UnitImplementations/UIComponents/Dashboard/Dashboard.ts
+++ UnitImplementations/UIComponents/Dashboard/Dashboard.ts
@@ -1,17 +1,14 @@
 import dashboard from '../../../Src/UIComponents/Dashboard/Dashboard'
+import dashboardView from '../../../Src/UIComponents/DashboardView/DashboardView'
+import {createElement} from '../../../Src/UIComponents/utils/utils'
+
 const username = 'User';
-const overview = document.createElement('div');
-overview.innerHTML = "Overview";
-const myDiaryFood = document.createElement('div');
-myDiaryFood.innerHTML = "Food";
-const myDiaryExercises = document.createElement('div');
-myDiaryExercises.innerHTML = "Exercises";
-const myGoals = document.createElement('div');
-myGoals.innerHTML = "Goals";
-const myWeights = document.createElement('div');
-myWeights.innerHTML = "Weights";
-const myProfile = document.createElement('div');
-myProfile.innerHTML = "Profile";
+const overview = dashboardView('Overview', createElement('div', [], 'overview content') as HTMLDivElement, `Hi ${username}!`);
+const myDiaryFood = dashboardView('Food', createElement('div', [], 'food content') as HTMLDivElement);
+const myDiaryExercises = dashboardView('Exercises', createElement('div', [], 'exercises content') as HTMLDivElement);
+const myGoals = dashboardView('Goals', createElement('div', [], 'goals content') as HTMLDivElement);
+const myWeights = dashboardView('Weights', createElement('div', [], 'weights content') as HTMLDivElement);
+const myProfile = dashboardView('My profile', createElement('div', [], 'profile content') as HTMLDivElement);
 document.body.appendChild(dashboard(username, {
     'overview': overview, 
     'diary-food': myDiaryFood, 
