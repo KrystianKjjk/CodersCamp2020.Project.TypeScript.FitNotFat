39a671f50e2f4c910f2455cd2b6d05cd6e4b6e31
Robert <robert.czwartosz@tlen.pl>
2021-02-10 21:16:03 +0100

    implement storing logged in user

--- Src/Logic/LoginButton/LoginButton.ts
+++ Src/Logic/LoginButton/LoginButton.ts
@@ -1,19 +1,22 @@
 import {readFromLocalStorage} from '../LocalStorage/LocalStorage';
 import {User} from '../../../Models/User.model';
+import {setLoggedInUser, getLoggedInUser} from '../../UIComponents/utils/utils';
 function loginButton(username: string, userDashboard: (user: User) => HTMLElement, failComp: HTMLParagraphElement): void {
-    const user = readFromLocalStorage(username) as User;
+    const loggedIn = getLoggedInUser();
+    const user = readFromLocalStorage(loggedIn ? loggedIn : username) as User | null;
     const loginBtn = this as HTMLButtonElement;
     if(user) {
+        setLoggedInUser(user.name);
         Array.from(document.body.children).forEach(element => {
             element.remove();
         });
         document.body.append(userDashboard(user));
-    } else {
-        loginBtn.parentElement.querySelectorAll('p').forEach((elem => {
-            elem.remove();
-        }))
-        loginBtn.parentElement.append(failComp);
+        return;
     }
+    loginBtn.parentElement.querySelectorAll('p').forEach((elem => {
+        elem.remove();
+    }))
+    loginBtn.parentElement.append(failComp);
 }
 
 export default loginButton;
