b58bfa20636103233dfc7f3c032534211f426a49
ireneusz <ireneusz.petryszak@gmail.com>
2021-02-10 19:12:43 +0100

    logic(UserInLocalStorage): implementation

--- Constants/consts.ts
+++ Constants/consts.ts
@@ -1,3 +1,5 @@
 export const NO_USER_ERROR = `User doesn't exist!`;
 export const NO_API_DETAILS_ERROR = `User doesn't contain API details.`;
 export const SAVED_CORRECTLY_CONFIRM = 'API DETAILS saved correctly!';
+
+export const KEY_LOGGED_USER = 'loggedInUserName';
