7a5961bfdb217bd9b5a55d5e7f5b26f69a00e784
ireneusz <ireneusz.petryszak@gmail.com>
2021-02-12 16:09:28 +0100

    components(Views): fix

--- UnitImplementations/UIComponents/Views/Views.ts
+++ UnitImplementations/UIComponents/Views/Views.ts
@@ -1,11 +1,41 @@
 import overviewComponent from "../../../Src/UIComponents/Overview/Overview";
-import { readFromLocalStorage } from "../../../Src/Logic/LocalStorage/LocalStorage";
-import { createElement, getLoggedInUser } from "../../../Src/UIComponents/utils/utils";
+import { readFromLocalStorage, saveInLocalStorage } from "../../../Src/Logic/LocalStorage/LocalStorage";
+import { createElement, getLoggedInUser, setLoggedInUser, clearLoggedInUser } from "../../../Src/UIComponents/utils/utils";
 import { ViewProfileInfo } from "../../../Src/UIComponents/ViewProfileInfo/ViewProfileInfo";
 import { ViewAPIKey } from "../../../Src/UIComponents/ViewAPIKey/ViewAPIKey";
 import { ViewMyWeights } from "../../../Src/UIComponents/ViewMyWeights/ViewMyWeights";
 import { ViewMyGoals } from "../../../Src/UIComponents/ViewMyGoals/ViewMyGoals";
 import { ViewOverview } from "../../../Src/UIComponents/ViewOverview/ViewOverview";
+import { ActivityLevel } from "../../../Models/ActivityLevel.model";
+import { User } from "../../../Models/User.model";
+import { WeeklyGoal } from "../../../Models/WeeklyGoal.model";
+
+const username = 'User3';
+
+/*
+const userObject: User = {
+  name: username,
+  gender: 'Male',
+  dateOfBirth: new Date(),
+  height: 177,
+  activityLevel: ActivityLevel.Low,
+  goalWeight: 70,
+  goals: [{
+    date: new Date(),
+    weeklyGoal: WeeklyGoal.Gain,
+    startWeight: 60,
+    endWeight: 71,
+    achieved: 'No'
+  }],
+  weights: [{date: new Date(), weight: 60}]
+};
+
+clearLoggedInUser();
+setLoggedInUser(username);
+saveInLocalStorage(username, userObject);
+*/
+
+const userObject = readFromLocalStorage(username);
 
 
 const viewProfileInfo = ViewProfileInfo();
@@ -14,13 +44,10 @@ const viewMyWeights = ViewMyWeights();
 const viewMyGoals = ViewMyGoals();
 const { viewOverview, viewOverviewContainerName } = ViewOverview();
 
-const username = getLoggedInUser();
-const userObject = readFromLocalStorage(username);
 
 //-------- to replace when finished --------------------------------------------
 import dashboard from '../../../Src/UIComponents/Dashboard/Dashboard'
 import dashboardView from '../../../Src/UIComponents/DashboardView/DashboardView'
-
 
 const myDiaryFood = dashboardView('Food', createElement('div', [], 'food content') as HTMLDivElement);
 const myDiaryExercises = dashboardView('Exercises', createElement('div', [], 'exercises content') as HTMLDivElement);
