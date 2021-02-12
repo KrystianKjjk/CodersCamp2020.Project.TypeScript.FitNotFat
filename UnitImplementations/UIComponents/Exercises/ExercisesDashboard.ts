3bb6f3503a8fd4791ee04b057c7f63dca2adf6f9
FIlip <filip.szwedo@gmail.com>
2021-02-12 18:53:08 +0100

    exercisesDashboard setup

---
+++ UnitImplementations/UIComponents/Exercises/ExercisesDashboard.ts
@@ -0,0 +1,5 @@
+import {createMyDiaryFoodDashboard} from '../../../Src/UIComponents/Exercises/ExercisesDashboard'
+
+const myExercisesDashboard = createMyDiaryFoodDashboard();
+
+document.body.append(myExercisesDashboard);
