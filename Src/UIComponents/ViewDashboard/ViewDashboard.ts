b7bdf13384f9cc0c813ad265fdcbeedac8d4d174
Konrad <kj.reczek@gmail.com>
2021-02-14 11:43:33 +0100

    Add exercises view

--- Src/UIComponents/ViewDashboard/ViewDashboard.ts
+++ Src/UIComponents/ViewDashboard/ViewDashboard.ts
@@ -1,18 +1,17 @@
-import { ViewProfileInfo } from "../../../Src/UIComponents/ViewProfileInfo/ViewProfileInfo";
-import { ViewAPIKey } from "../../../Src/UIComponents/ViewAPIKey/ViewAPIKey";
-import { ViewMyWeights } from "../../../Src/UIComponents/ViewMyWeights/ViewMyWeights";
-import { ViewMyGoals } from "../../../Src/UIComponents/ViewMyGoals/ViewMyGoals";
-import { ViewOverview } from "../../../Src/UIComponents/ViewOverview/ViewOverview";
-import { ViewMyFood } from "../../../Src/UIComponents/ViewMyFood/ViewMyFood";
-import dashboard from '../../../Src/UIComponents/Dashboard/Dashboard';
-import dashboardView from '../../../Src/UIComponents/DashboardView/DashboardView';
-import { createElement } from '../../UIComponents/utils/utils';
-import {getLoggedInUser} from '../../UIComponents/utils/utils';
+import { ViewProfileInfo } from "../../UIComponents/ViewProfileInfo/ViewProfileInfo";
+import { ViewAPIKey } from "../../UIComponents/ViewAPIKey/ViewAPIKey";
+import { ViewMyWeights } from "../../UIComponents/ViewMyWeights/ViewMyWeights";
+import { ViewMyGoals } from "../../UIComponents/ViewMyGoals/ViewMyGoals";
+import { ViewOverview } from "../../UIComponents/ViewOverview/ViewOverview";
+import { ViewMyFood } from "../../UIComponents/ViewMyFood/ViewMyFood";
+import { ViewExercises } from "../../UIComponents/ViewExercises/ViewExercises";
+import dashboard from "../../UIComponents/Dashboard/Dashboard";
+import { getLoggedInUser } from "../../UIComponents/utils/utils";
 
 export function viewDashboard():HTMLDivElement{ 
     const username = getLoggedInUser()
     
-    const myDiaryExercises = dashboardView('Exercises', createElement('div', [], 'exercises content') as HTMLDivElement);
+    const myDiaryExercises = ViewExercises();
     const viewProfileInfo = ViewProfileInfo();
     const viewApiKey = ViewAPIKey();
     const viewMyWeights = ViewMyWeights();
