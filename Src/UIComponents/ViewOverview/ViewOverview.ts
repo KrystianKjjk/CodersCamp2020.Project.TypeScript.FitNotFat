c704524ba049d50d930851dfb817f03f4401e470
FIlip <filip.szwedo@gmail.com>
2021-02-14 19:42:10 +0100

    fix for the fix

--- Src/UIComponents/ViewOverview/ViewOverview.ts
+++ Src/UIComponents/ViewOverview/ViewOverview.ts
@@ -1,4 +1,4 @@
-import { overviewComponent, OVERVIEW_CONTAINER_CLASS_NAME } from "../Overview/Overview";
+import { OVERVIEW_CONTAINER_CLASS_NAME } from "../Overview/Overview";
 import dashboardView from "../DashboardView/DashboardView";
 import { createElement, getLoggedInUser } from "../utils/utils";
 import { readFromLocalStorage } from "../../Logic/LocalStorage/LocalStorage";
@@ -14,7 +14,6 @@ export function ViewOverview() {
     const welcomeHeader = `Hi <span>${userObject.name}</span>, welcome back!`;
     const overviewComponentContainer = createElement('div', OVERVIEW_CONTAINER_CLASS_NAME) as HTMLDivElement;
     overviewView = dashboardView(VIEW_NAME.Overview, overviewComponentContainer, welcomeHeader);
-    overviewComponent(userObject, OVERVIEW_CONTAINER_CLASS_NAME);
   }
   else {
     overviewView = dashboardView(VIEW_NAME.Overview, (createElement('div', 'error-view', 'Something went wrong') as HTMLDivElement));
